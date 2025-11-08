4e836560fe4faf5ade95d439161c59b8b147a6ee
Rost445 <rostislavgritsyk@gmail.com>
2025-11-08 17:42:15 +0200

    js add url

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1847,17 +1847,16 @@
     <div id="preloader"></div>
 
     <!-- Vendor JS Files -->
-    <script src="front/assetsvendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="front/assets/vendor/php-email-form/validate.js"></script>
-    <script src="front/assets/vendor/aos/aos.js"></script>
-    <script src="front/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
-    <script src="front/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-    <script src="front/assets/vendor/swiper/swiper-bundle.min.js"></script>
-    <script src="front/assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="{{ ('front/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ ('front/assets/vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ ('front/assets/vendor/aos/aos.js') }}"></script>
+    <script src="{{ ('front/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js') }}"></script>
+    <script src="{{ ('front/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ ('front/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="{{ ('front/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
 
-    <!-- Main JS File -->
-    <script src="front/assets/js/main.js"></script>
-
+   
+    <script src="{{ ('front/assets/js/main.js') }}"></script>
 </body>
 
 </html>
